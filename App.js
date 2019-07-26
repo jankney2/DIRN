5d223facc940a322fb46092453e63ae31d845374
Jacob Ankney <crosswalkastronaut@Jacobs-MBP-2.app.byu.edu>
2019-07-26 14:22:22 -0600

    flatlist starting to work

--- App.js
+++ App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import Login from './Components/Login';
 import HomePage from './Components/HomePage'
+import Properties from './Components/Properties';
 
 
 
 const App = createStackNavigator({
   
     login: Login,
-    homepage:HomePage
+    homepage:HomePage, 
+    properties:Properties
   
 },
 {
-initialRouteName:'login'
+initialRouteName:'properties'
 });
 
 export default createAppContainer(App)
--- Components/Properties.js
+++ Components/Properties.js
@@ -1,15 +1,47 @@
-import React, { Component } from 'react';
-import {View, Text} from 'react-native'
-
+import React, { Component } from "react";
+import { View, Text, StyleSheet, FlatList, Alert } from "react-native";
+import axios from "axios";
+import { ListItem } from "react-native-elements";
 
 export default class Properties extends Component {
-    state = {  }
-    render() {
-        return (
-            <View>
+  state = {
+    userProperties: []
+  };
 
-                
-            </View>
-        );
-    }
+  componentDidMount() {
+    axios
+      .get("https://dropin.business/api/userProperties/14")
+      .then(res => {
+        this.setState({
+          userProperties: res.data
+        });
+      })
+      .catch(err => Alert.alert(err));
+  }
+
+  render() {
+    return (
+      <View style={styles.contain}>
+        <Text>Properties</Text>
+
+          <FlatList
+            data={this.state.userProperties}
+            renderItem={({ item }) => {
+              return <Text style={{ textAlign: "center" }}>{item.street}</Text>;
+              
+                  
+            }}
+            keyExtractor={item => item.property_id.toString()}
+          />
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  contain: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
